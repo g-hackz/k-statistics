66ae1361e6017d9e44eda21b6d934ff7dee5469d
ghackz <gobokel@gmail.com>
2017-02-01 20:44:47 +0100

    Two new functions added, README also added.

---
+++ README.md
@@ -0,0 +1,30 @@
+# K-Statistics
+
+K-Statistics is a simple module for calculating basic statistical measures like:
+
+* Mean
+* Median
+* Mode
+* Variance
+* Standard Deviation
+
+## Prerequisites
+
+You need python 2.7 to use this module
+
+## Installation
+
+Clone the GitHub repository and run the **setup.py** file.
+
+```
+$ git clone https://github.com/g-hackz/k-statistics.git
+$ cd k-statistics
+$ python setup.py
+```
+
+## Usage
+
+```
+>>> import k-statistics
+>>> print k-statistics.get_mean([4, 7, 10])
+```
--- k_statistics.py
+++ k_statistics.py
@@ -1,3 +1,8 @@
+from math import sqrt
+
+def square(number):
+    '''return the square of a number'''
+    return number * number
 
 def get_mean(dataset):
     '''returns the mean of the given data set'''
@@ -33,11 +38,38 @@ def get_mode(dataset):
 
     return mode
 
+def freq_dist_mean(dataset, freqset):
+    '''calculate the mean of a frequency distribution given the data set and frequencies'''
+    fx = [dataset[i] * freqset[i] for i in xrange(len(dataset))]
+    efx = sum(fx)
+    ef = sum(freqset)
+    return float(efx)/ef
+
+def get_var_and_sd(dataset):
+    '''calculate the variance and standard deviation of a frequency distribution given the data set and frequencies'''
+    mean = get_mean(dataset)
+    smx = [i-mean for i in dataset]
+    smx_square = [square(i) for i in smx]
+    sum_of_squares = sum(smx_square)
+    variance = float(sum_of_squares)/len(dataset)
+    return variance, sqrt(variance)
+
+
+
+
 # Usage
 
-print 'Mean = ', get_mean([4, 6, 6, 7, 7, 7, 8, 10])
+print 'Mean: ', get_mean([4, 6, 6, 7, 7, 7, 8, 10])
 
-print 'Median = ', get_median([2, 1, 9, 6, 7, 14, 11, 11, 12])
-print 'Median = ', get_median([24, 12, 16, 22, 10, 25])
+print 'Median: ', get_median([2, 1, 9, 6, 7, 14, 11, 11, 12])
+print 'Median: ', get_median([24, 12, 16, 22, 10, 25])
 
-print 'Mode = ', get_mode([1, 1, 4, 4, 5, 6, 8, 8, 8, 9])
+print 'Mode: ', get_mode([1, 1, 4, 4, 5, 6, 8, 8, 8, 9])
+
+FREQ_DISTRIBUTION = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+FREQUENCIES = [0, 0, 1, 0, 1, 7, 16, 8, 3, 1, 1]
+print 'Mean of the frequency distribution is: %.2f' % freq_dist_mean(FREQ_DISTRIBUTION, FREQUENCIES)
+
+var, std_dev = get_var_and_sd([4, 7, 10])
+print 'Variance: %.2f' % var
+print 'Standard deviation: %.3f' % std_dev
